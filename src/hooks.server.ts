53f9d7bb1e47985cdca4fd24fb55bd3a28017a37
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-02-10 10:21:56 -0300

    Remove auth middleware and protected route
    Delete the cookie-based authentication system and the /protected
    route, simplifying hooks.server.ts to only use Sentry.

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,5 @@
 import * as Sentry from '@sentry/sveltekit';
-import { redirect, type Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
-
-import { authenticateUser } from '$lib/server/auth';
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -10,20 +7,6 @@ Sentry.init({
   tracesSampleRate: 1.0
 });
 
-const authHandle: Handle = async ({ event, resolve }) => {
-  event.locals.user = authenticateUser(event);
-
-  if (event.url.pathname.startsWith('/protected')) {
-    if (!event.locals.user) {
-      throw redirect(303, '/');
-    }
-  }
-
-  const response = await resolve(event);
-
-  return response;
-};
-
-export const handle = sequence(Sentry.sentryHandle(), authHandle);
+export const handle = sequence(Sentry.sentryHandle());
 
 export const handleError = Sentry.handleErrorWithSentry();
